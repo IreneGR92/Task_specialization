c6b90866bf413a8f37b7e6ec65906403291d8138
IreneGR92 <irene.garcia@iee.unibe.ch>
2022-12-28 11:57:34 +0100

    remove todo

--- ubelix.sh
+++ ubelix.sh
@@ -29,7 +29,7 @@
 
 # For array jobs
 # Indicate how many input files you have
-#SBATCH --array=0-69 //TODO: Number of runs, remember to change!
+#SBATCH --array=0-69
 
 #### Your shell commands below this line ####
 
