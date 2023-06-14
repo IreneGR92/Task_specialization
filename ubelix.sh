2de7222ccb956571cf82101158e68595cea0d54f
IreneGR92 <igaru.13@gmail.com>
2023-06-14 13:59:46 -0400

    ubelix scramble context

--- ubelix.sh
+++ ubelix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # You must specify a valid email address!
-#SBATCH --mail-user=irene.garcia@unibe.ch
+#SBATCH --mail-user=ig17c521@campus.unibe.ch
 
 # Mail on NONE, BEGIN, END, FAIL, REQUEUE, ALL
 #SBATCH --mail-type=end,fail
