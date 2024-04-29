04b6f83d2791bc07db0829a34b1f402afce059ce
IreneGR92 <igaru.13@gmail.com>
2024-04-29 16:50:21 -0400

    noGA yml

--- ubelix.sh
+++ ubelix.sh
@@ -29,17 +29,49 @@
 
 # For array jobs
 # Indicate how many input files you have
-#SBATCH --array=0-66
+#SBATCH --array=0-35
 
 #### Your shell commands below this line ####
 
 declare -a arr=(
 
-"NoDOL_m02-Xh5-Yh01_default.yml"
+"DOL_m01-Xh3-Yh01_NoGA-under.yml"
+"DOL_m01-Xh5-Yh01_NoGA-under.yml"
+"DOL_m01-Xh7-Yh01_NoGA-under.yml"
+"DOL_m02-Xh3-Yh01_NoGA-under.yml"
+"DOL_m02-Xh5-Yh01_NoGA-under.yml"
+"DOL_m02-Xh7-Yh01_NoGA-under.yml"
+"DOL_m03-Xh3-Yh01_NoGA-under.yml"
+"DOL_m03-Xh5-Yh01_NoGA-under.yml"
+"DOL_m03-Xh7-Yh01_NoGA-under.yml"
+"NoDOL_m01-Xh3-Yh01_NoGA-under.yml"
+"NoDOL_m01-Xh5-Yh01_NoGA-under.yml"
+"NoDOL_m01-Xh7-Yh01_NoGA-under.yml"
+"NoDOL_m02-Xh3-Yh01_NoGA-under.yml"
+"NoDOL_m02-Xh5-Yh01_NoGA-under.yml"
+"NoDOL_m02-Xh7-Yh01_NoGA-under.yml"
+"NoDOL_m03-Xh3-Yh01_NoGA-under.yml"
+"NoDOL_m03-Xh5-Yh01_NoGA-under.yml"
+"NoDOL_m03-Xh7-Yh01_NoGA-under.yml"
 
-"NoDOL_m03-Xh5-Yh01_NoRelatedness.yml"
-"NoDOL_m03-Xh7-Yh01_NoRelatedness.yml"
-
+"DOL_m01-Xh3-Yh01_NoGA-upper.yml"
+"DOL_m01-Xh5-Yh01_NoGA-upper.yml"
+"DOL_m01-Xh7-Yh01_NoGA-upper.yml"
+"DOL_m02-Xh3-Yh01_NoGA-upper.yml"
+"DOL_m02-Xh5-Yh01_NoGA-upper.yml"
+"DOL_m02-Xh7-Yh01_NoGA-upper.yml"
+"DOL_m03-Xh3-Yh01_NoGA-upper.yml"
+"DOL_m03-Xh5-Yh01_NoGA-upper.yml"
+"DOL_m03-Xh7-Yh01_NoGA-upper.yml"
+"NoDOL_m01-Xh3-Yh01_NoGA-upper.yml"
+"NoDOL_m01-Xh5-Yh01_NoGA-upper.yml"
+"NoDOL_m01-Xh7-Yh01_NoGA-upper.yml"
+"NoDOL_m02-Xh3-Yh01_NoGA-upper.yml"
+"NoDOL_m02-Xh5-Yh01_NoGA-upper.yml"
+"NoDOL_m02-Xh7-Yh01_NoGA-upper.yml"
+"NoDOL_m03-Xh3-Yh01_NoGA-upper.yml"
+"NoDOL_m03-Xh5-Yh01_NoGA-upper.yml"
+"NoDOL_m03-Xh7-Yh01_NoGA-upper.yml"
 
 		)
 
