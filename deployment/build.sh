2cc9037095029a3a3e3b9f0192e07a244deefc24
tobak <tobak@protonmail.com>
2025-09-02 22:18:45 +0200

    build.sh

--- deployment/build.sh
+++ deployment/build.sh
@@ -4,12 +4,14 @@
 name=${PWD##*/}
 echo $1
 
+parameter_name = ${1%.yml}
+
 build_folder=$2  # Get the build folder name from the second argument
 
 cd "../$build_folder"
 
 ./App "../parameters/$1"
 
-mv *"${1%.yml}"* /home/odin/TresoritDrive/Results/Task_specialization_results
+mv *"$parameter_name"* /home/odin/TresoritDrive/Results/Task_specialization_results
 
 echo "done"
